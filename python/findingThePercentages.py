7ea28379e4eb81f11df90a57006d6e3fc6cbb582
Elena Corpus <54608775+elenatheresa@users.noreply.github.com>
2024-01-14 14:34:48 -0500

    Create findingThePercentages.py

---
+++ python/findingThePercentages.py
@@ -0,0 +1,27 @@
+'''
+The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+
+The first line contains the integer n, the number of students' records. The next n lines contain the names and marks obtained by a student, each value separated by a space. The final line contains query_name, the name of a student to query.
+
+Print one line: The average of the marks obtained by the particular student correct to 2 decimal places.
+'''
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    total = sum(student_marks[query_name])
+    lengthTotal = len(student_marks[query_name])
+    averageTotal = total / lengthTotal
+
+    
+    a = "{:.2f}".format(averageTotal)
+    
+    print(a)
